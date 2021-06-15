e6bf183b266d665a164327d4d28acbded15affe4
ove <isiful.ovi@gmail.com>
2021-06-15 16:23:37 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            income: 0,
+            expense: 0,
+            other: 0,
+            type: '',
+            amount: ''
+        }
+    },
+    methods: {
+        optionSelection(event) {
+            this.type = event.target.value;
+        },
+        save() {
+            if (this.type === '' && this.amount === '') {
+                alert("Please Select any option")
+            }
+            else {
+                if (this.type === 'income') {
+                    this.income = this.income + parseInt(this.amount);
+                }
+                if (this.type === 'expense') {
+                    this.expense = this.expense + parseInt(this.amount);
+
+                }
+                if (this.type === 'other') {
+                    this.other = this.other + parseInt(this.amount);
+
+                }
+                this.clear();
+            }
+        },
+        clear() {
+            this.type = '';
+            this.amount = '';
+        }
+    }
+})
+
+app.mount("#app")
